ed71bd57a75b50f646680217b2fb63bce3acecd9
buldOo <killian.gay28@gmail.com>
2022-11-21 13:43:54 +0100

    Fix position feature

--- js/src/components/usagesHandler.js
+++ js/src/components/usagesHandler.js
@@ -1,16 +1,18 @@
 
 export class usagesHandler {
     static positionLine() {
-        let lines = document.querySelectorAll('.images-container');
+        let lines = document.querySelectorAll('.svg-lines line');
         let images = document.querySelectorAll('.usage-img');
+
+        let totalHeight = window.document.documentElement.getBoundingClientRect().height;
+        document.querySelector('.full-page-div').style.height = totalHeight + "px";
 
         usagesHandler.calculPosition(lines, images);
         var observer = new ResizeObserver(function(entries) {
+            let totalHeight = window.document.documentElement.getBoundingClientRect().height;
+            document.querySelector('.full-page-div').style.height = totalHeight + "px";
             usagesHandler.calculPosition(lines, images);
         })
-
-
-
         observer.observe(window.document.body);
         
     }
@@ -21,21 +23,17 @@ export class usagesHandler {
         lines.forEach((line, index) => {
             center = (images[index].getBoundingClientRect().width / 2);
             bottom = images[index].getBoundingClientRect().height;
-            height = (images[index + 1].getBoundingClientRect().top) - (images[index].getBoundingClientRect().bottom);
+            height = (images[index + 1].getBoundingClientRect().top) - (images[index].getBoundingClientRect().top + window.scrollY);
             left = (images[index + 1].getBoundingClientRect().left + images[index + 1].getBoundingClientRect().width / 2) - (images[index].getBoundingClientRect().left + center);
             pythagore = Math.hypot(height, left);
             line.style.top = bottom + "px";
             line.style.left = center + "px";
             line.style.width = (pythagore + 20) + "px";
-            switch (index) {
-                case 0:
-                case 2:
-                    line.style.transform = "rotate(26deg)";
-                    break;  
-                case 1: 
-                    line.style.transform = "rotate(148deg)";
-                    break;
-            }
+
+            line.setAttribute('x1', images[index].getBoundingClientRect().left + center);
+            line.setAttribute('y1', images[index].getBoundingClientRect().bottom + window.scrollY);
+            line.setAttribute('x2', images[index + 1].getBoundingClientRect().left + center);
+            line.setAttribute('y2', images[index + 1].getBoundingClientRect().top + window.scrollY);
         });
     }
 }
